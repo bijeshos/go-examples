93001bb2a76ab4f1ffd10443aa8b7838370d3a46
Bijesh O S <bijeshos@users.noreply.github.com>
2020-02-15 16:58:35 +0530

    updated main.go to invoke recently added functions

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/bijeshos/go.examples/basics"
+	"github.com/bijeshos/go.examples/variables"
+)
 
 func main() {
-	fmt.Println("Hello from main")
+	basics.PrintMessage("test message")
+	fmt.Println("Random number is: ", basics.GetRandomNumber())
+	fmt.Println("Value of Pi is: ", basics.GetPi())
+
+	fmt.Println("Sum of 10 and 20 is: ", basics.Add(10, 20))
+	a, b := basics.Swap(50, 60)
+	fmt.Println("Input: 50,60 | Output: ", a, b)
+
+	variables.PrintDefaultVariableValues()
 }
