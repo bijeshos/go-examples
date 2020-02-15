3bc2e47f2f784ffeb76b645ea05ddd3257a70fd6
Bijesh O S <bijeshos@users.noreply.github.com>
2020-02-15 16:56:48 +0530

    added basic math functions

---
+++ basics/math.go
@@ -0,0 +1,26 @@
+package basics
+
+import (
+	"math"
+	"math/rand"
+)
+
+//GetRandomNumber returns a random number
+func GetRandomNumber() int {
+	return rand.Int()
+}
+
+//GetPi returns value of PI
+func GetPi() float64 {
+	return math.Pi
+}
+
+//Add adds two values and returns the sum
+func Add(x int, y int) int {
+	return x + y
+}
+
+//Swap swaps two numbers
+func Swap(x, y int) (int, int) {
+	return y, x
+}
