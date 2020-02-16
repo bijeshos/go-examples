40098b53c0e2ba30f280eb4769e88133ed32c152
Bijesh O S <bijeshos@users.noreply.github.com>
2020-02-16 21:49:11 +0530

    added function values example

---
+++ functions/function_examples.go
@@ -0,0 +1,21 @@
+package functions
+
+import (
+	"fmt"
+	"math"
+)
+
+func compute(fn func(float64, float64) float64) float64 {
+	return fn(3, 4)
+}
+
+//FunctionValues example for function being passed as values
+func FunctionValues() {
+	hypot := func(x, y float64) float64 {
+		return math.Sqrt(x*x + y*y)
+	}
+	fmt.Println(hypot(5, 12))
+
+	fmt.Println(compute(hypot))
+	fmt.Println(compute(math.Pow))
+}
