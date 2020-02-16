43a6f820a1375a74b7104fee8816783a026fccea
Bijesh O S <bijeshos@users.noreply.github.com>
2020-02-16 09:17:48 +0530

    added conditional examples

---
+++ flowcontrol/if_example.go
@@ -0,0 +1,25 @@
+package flowcontrol
+
+import (
+	"fmt"
+	"math"
+)
+
+//Sqrt if example
+func Sqrt(x float64) string {
+	if x < 0 {
+		return Sqrt(-x) + "i"
+	}
+	return fmt.Sprint(math.Sqrt(x))
+}
+
+//Pow if-else example
+func Pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	} else {
+		fmt.Printf("%g >= %g\n", v, lim)
+	}
+	// can't use v here, though
+	return lim
+}
