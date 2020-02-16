83e67f8071d552520e0e5a29b21e0f80b6b7d249
Bijesh O S <bijeshos@users.noreply.github.com>
2020-02-16 21:49:57 +0530

    added function closure example

--- functions/function_examples.go
+++ functions/function_examples.go
@@ -19,3 +19,22 @@ func FunctionValues() {
 	fmt.Println(compute(hypot))
 	fmt.Println(compute(math.Pow))
 }
+
+func adder() func(int) int {
+	sum := 0
+	return func(x int) int {
+		sum += x
+		return sum
+	}
+}
+
+//FunctionClosure example for function closure
+func FunctionClosure() {
+	pos, neg := adder(), adder()
+	for i := 0; i < 10; i++ {
+		fmt.Println(
+			pos(i),
+			neg(-2*i),
+		)
+	}
+}
