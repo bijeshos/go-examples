c7520945ecedec39e912968be3c23f4643fec031
Bijesh O S <bijeshos@users.noreply.github.com>
2020-02-17 18:05:02 +0530

    added interface values example

---
+++ interfaces/interface_values.go
@@ -0,0 +1,46 @@
+package interfaces
+
+import (
+	"fmt"
+	"math"
+)
+
+//I interface values example
+type I interface {
+	M()
+}
+
+//T interface values example
+type T struct {
+	S string
+}
+
+//M interface values example
+func (t *T) M() {
+	fmt.Println(t.S)
+}
+
+//F interface values example
+type F float64
+
+//M interface values example
+func (f F) M() {
+	fmt.Println(f)
+}
+
+//InterfaceValuesExample interface values example
+func InterfaceValuesExample() {
+	var i I
+
+	i = &T{"Hello"}
+	describe(i)
+	i.M()
+
+	i = F(math.Pi)
+	describe(i)
+	i.M()
+}
+
+func describe(i I) {
+	fmt.Printf("(%v, %T)\n", i, i)
+}
