504f953cf1cab9fc2db5b4f083a3762789148699
Bijesh O S <bijeshos@users.noreply.github.com>
2020-02-22 17:07:26 +0530

    added error example

---
+++ erros/errors-5.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	const name, id = "bimmler", 17
+	err := fmt.Errorf("user %q (id %d) not found", name, id)
+	if err != nil {
+		fmt.Print(err)
+	}
+}
+
+//reference: https://golang.org/pkg
