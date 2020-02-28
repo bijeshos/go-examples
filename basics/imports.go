dbb092f18a5a9faaa5148c869eb7c4e073de7e30
Bijesh O S <bijeshos@users.noreply.github.com>
2020-02-28 23:25:57 +0530

    added additional comments

--- basics/imports.go
+++ basics/imports.go
@@ -9,4 +9,8 @@ func main() {
 	fmt.Printf("Square root of 7 is: %g \n", math.Sqrt(7))
 }
 
+/*
+A basic program to demonstrate package import: fmt and math
+*/
+
 //reference: https://tour.golang.org/
