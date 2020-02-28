7074220a195d16f1bf90cda37f395def8fbe9521
Bijesh O S <bijeshos@users.noreply.github.com>
2020-02-28 23:26:09 +0530

    added additional comments

--- basics/packages.go
+++ basics/packages.go
@@ -9,4 +9,7 @@ func main() {
 	fmt.Println("My favorite number is", rand.Intn(10))
 }
 
+/*
+A basic program to demonstrate package main and import packages
+*/
 //reference: https://tour.golang.org/
