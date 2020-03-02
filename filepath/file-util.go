275ecfea30544cd03d87f590eb12d5fc3ac3ad89
Bijesh O S <bijeshos@users.noreply.github.com>
2020-03-02 13:48:48 +0530

    changed directory, add more fileinfo related checks

--- filepath/file-util.go
+++ filepath/file-util.go
@@ -14,8 +14,22 @@ func Read(srcDir string) {
 
 	err := filepath.Walk(srcDir, func(path string, info os.FileInfo, err error) error {
 		fmt.Println("----")
-		fmt.Println("paht", path)
-		fmt.Println("fileinfo", info)
+		fmt.Println("path: ", path)
+		fmt.Println("file-info:isdir: ", info.IsDir())
+		fmt.Println("file-info:mode: ", info.Mode())
+		fmt.Println("file-info:mod time: ", info.ModTime())
+		fmt.Println("file-info:name: ", info.Name())
+		fmt.Println("file-info:sys: ", info.Sys())
+
+		fmt.Println("file-info:ext: ", filepath.Ext(path))
+
+		if filepath.Ext(path) == ".txt" {
+			fmt.Println("skipping txt")
+			return nil
+		} else {
+
+		}
+
 		files = append(files, path)
 		return nil
 	})
