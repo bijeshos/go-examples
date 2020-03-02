388b8bf3f0f756914f3057602bccf0b1e2673abb
Bijesh O S <bijeshos@users.noreply.github.com>
2020-03-02 13:51:17 +0530

    added os.read() example

---
+++ os/file-os-readdir.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	dirname := "."
+
+	//the files returned are not in sorted order
+	f, err := os.Open(dirname)
+	if err != nil {
+		log.Fatal(err)
+	}
+	files, err := f.Readdir(-1)
+	f.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		fmt.Println(file.Name())
+	}
+}
