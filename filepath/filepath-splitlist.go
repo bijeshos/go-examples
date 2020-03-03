215d3a4582f87c9f6f6a85751ba5d41f5608d307
Bijesh O S <bijeshos@users.noreply.github.com>
2020-03-03 18:15:34 +0530

    added filepath-splitlist example

---
+++ filepath/filepath-splitlist.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"path/filepath"
+)
+
+func main() {
+	//splitlist splits a list of paths joined by OS specific separator (: or ;)
+	fmt.Println(filepath.SplitList("/a/b/c:/usr/bin"))
+}
