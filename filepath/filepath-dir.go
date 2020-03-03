0e3437272cf891dcfc1d1e5fc8cced989ceabf34
Bijesh O S <bijeshos@users.noreply.github.com>
2020-03-03 18:07:27 +0530

    added filepath-dir example

---
+++ filepath/filepath-dir.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"path/filepath"
+)
+
+func main() {
+	//Dir returns the last directory in the path, without the last slashes
+
+	fmt.Println(filepath.Dir("/foo/bar/baz.js"))
+	fmt.Println(filepath.Dir("/foo/bar/baz"))
+	fmt.Println(filepath.Dir("/foo/bar/baz/"))
+	fmt.Println(filepath.Dir("/dirty//path///"))
+	fmt.Println(filepath.Dir("dev.txt"))
+	fmt.Println(filepath.Dir("../todo.txt"))
+	fmt.Println(filepath.Dir(".."))
+	fmt.Println(filepath.Dir("."))
+	fmt.Println(filepath.Dir("/"))
+	fmt.Println(filepath.Dir(""))
+
+}
