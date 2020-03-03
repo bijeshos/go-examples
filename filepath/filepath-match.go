c693a452af6ed28ad91b71f74189426c15e3a494
Bijesh O S <bijeshos@users.noreply.github.com>
2020-03-03 18:43:58 +0530

    minor restructuring

--- filepath/filepath-match.go
+++ filepath/filepath-match.go
@@ -9,9 +9,17 @@ func main() {
 
 	//Match reports whether the name matches file pattern
 	//returns boolean and error
-	fmt.Println(filepath.Match("/home/catch/*", "/home/catch/foo"))
-	fmt.Println(filepath.Match("/home/catch/*", "/home/catch/foo/bar"))
-	fmt.Println(filepath.Match("/home/?opher", "/home/gopher"))
-	fmt.Println(filepath.Match("/home/\\*", "/home/*"))
+	inputs := [][]string{
+		{"/home/catch/*", "/home/catch/foo"},
+		{"/home/catch/*", "/home/catch/foo/bar"},
+		{"/home/?opher", "/home/gopher"},
+		{"/home/\\*", "/home/*"},
+	}
+	for _, row := range inputs {
+		match, _ := filepath.Match(row[0], row[1])
+		fmt.Printf("pattern: %q | name: %q | match: %t\n", row[0], row[1], match)
+	}
 
 }
+
+//reference: https://golang.org/pkg
