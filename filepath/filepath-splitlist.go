f342f7d255e070b4eacb3ca3d6291cb96bfdf3c4
Bijesh O S <bijeshos@users.noreply.github.com>
2020-03-03 18:43:24 +0530

    added reference

--- filepath/filepath-splitlist.go
+++ filepath/filepath-splitlist.go
@@ -9,3 +9,5 @@ func main() {
 	//splitlist splits a list of paths joined by OS specific separator (: or ;)
 	fmt.Println(filepath.SplitList("/a/b/c:/usr/bin"))
 }
+
+//reference: https://golang.org/pkg
