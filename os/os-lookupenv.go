e0e3b56976ba82ed39a6933afc3d4e9f5c00c788
Bijesh O S <bijeshos@users.noreply.github.com>
2020-03-04 10:06:13 +0530

    added os-lookupenv example

---
+++ os/os-lookupenv.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	printEnv := func(key string) {
+		val, ok := os.LookupEnv(key)
+		if !ok {
+			fmt.Printf("%s not set\n", key)
+		} else {
+			fmt.Printf("%s=%s\n", key, val)
+		}
+	}
+
+	printEnv("GOROOT")
+	printEnv("GOPATH")
+	printEnv("GOBIN")
+
+}
