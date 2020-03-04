b2f2608660a43003b4520484d2eea2f953180dc2
Bijesh O S <bijeshos@users.noreply.github.com>
2020-03-04 10:02:11 +0530

    added additional comments

--- concurrency/buffered-channels.go
+++ concurrency/buffered-channels.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func main() {
+	// this channel has a buffer size of 2
+	// sends are blocked when channel is full
+	// receives are blocked when channel is empty
 	ch := make(chan int, 2)
 	ch <- 1
 	ch <- 2
