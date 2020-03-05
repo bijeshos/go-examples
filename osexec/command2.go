951ff69cc286f61e7d09cdb029dc24738ac553af
Bijesh O S <bijeshos@users.noreply.github.com>
2020-03-05 21:34:17 +0530

    added os-command example

---
+++ osexec/command2.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	cmd := exec.Command("prog")
+	cmd.Env = append(os.Environ(),
+		"FOO=duplicate_value", // ignored
+		"FOO=actual_value",    // this value is used
+	)
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+//reference: https://golang.org/pkg
