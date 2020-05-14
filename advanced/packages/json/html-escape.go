1985abdd943659dd085a5acdaa3e6d18b0347bdf
Bijesh O S <bijeshos@users.noreply.github.com>
2020-05-14 15:02:08 +0530

    refactoring

---
+++ advanced/packages/json/html-escape.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"os"
+)
+
+func main() {
+	var out bytes.Buffer
+	json.HTMLEscape(&out, []byte(`{"Name":"<b>HTML content</b>"}`))
+	out.WriteTo(os.Stdout)
+}
+
+//https://golang.org/pkg
